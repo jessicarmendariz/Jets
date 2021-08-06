8a80df7facf776d0167ecebf745dabf58b41411a
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-08-06 16:06:45 -0600

    First Push

---
+++ src/com/skilldistillery/jets/app/JetsApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entities.AirField;
+//CMD SHIFT "o"
+
+public class JetsApp {
+
+	private AirField airField;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
